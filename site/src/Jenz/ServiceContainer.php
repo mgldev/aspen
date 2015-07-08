6c3fa8a904839b5437402a60b687e0e641d82f77
Jen Cockerill <jenc@magmadigital.co.uk>
2015-07-08 19:13:27 +0100

    Add: service container, and unit tests

---
+++ site/src/Jenz/ServiceContainer.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Jenz;
+
+class ServiceContainer extends \ArrayObject {
+
+	protected $cache = [];
+
+	public function get($name) {
+
+		if (!isset($this[$name])) {
+			throw new \Exception('Item "' . $name . '" could not be found in the Service Container"');
+		}
+
+		if (is_callable($this[$name])) {
+
+			if (isset($this->cache[$name])) {
+				return $this->cache[$name];
+			}
+
+			$item = call_user_func($this[$name]);
+			$this->cache[$name] = $item;
+			return $item;
+		}
+
+		return $this[$name];
+
+	}
+}
